f11e3b1a85b722a15907112be19f9cc005a8622a
icantiemyshoe <icantiemyshoe@yahoo.com>
2016-03-02 04:24:32 +0000

    adding the email_support.py file with comments using simple smtp rather than MIME

--- email_support.py
+++ email_support.py
@@ -3,37 +3,32 @@
 # Import smtplib for the actual sending function
 import smtplib
 
-# Here are the email package modules we'll need
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
+# Import the email modules we'll need
+from email.mime.text import MIMEText
 
-COMMASPACE = ', '
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+fp = open(textfile, 'rb')
+# Create a text/plain message
+msg = MIMEText(fp.read())
+fp.close()
 
-# Create the container (outer) email message.
-msg = MIMEMultipart()
-msg['Subject'] = 'Our family reunion'
 # me == the sender's email address
-# family = the list of all recipients' email addresses
+# you == the recipient's email address
+msg['Subject'] = 'The contents of %s' % textfile
 msg['From'] = me
-msg['To'] = COMMASPACE.join(family)
-msg.preamble = 'Our family reunion'
+msg['To'] = you
 
-# Assume we know that the image files are all in PNG format
-for file in pngfiles:
-    # Open the files in binary mode.  Let the MIMEImage class automatically
-    # guess the specific image type.
-    fp = open(file, 'rb')
-    img = MIMEImage(fp.read())
-    fp.close()
-    msg.attach(img)
- 
 # This will allow the individuals to be put on the same array and then sent throught smtp once    
 # to = ["person1@example.com", "person2@example.com", "person3@example.com"]
 # msg['To'] = ",".join(to)
 # s.sendmail(me, to, msg.as_string())    
 
-# Send the email via our own SMTP server.
+# Send the message via our own SMTP server, but don't include the
+# envelope header.
 s = smtplib.SMTP('localhost')
-s.sendmail(me, family, msg.as_string())
+s.sendmail(me, [you], msg.as_string())
 s.quit()
+
+
 
